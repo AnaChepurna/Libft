d7e0ef5ebb87a2f9f199f305a197049b3c97236f
Anastasia CHEPURNA <achepurn@e3r7p3.unit.ua>
2017-10-30 10:08:40 +0200

    # Libft

---
+++ libft.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achepurn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/26 16:27:14 by achepurn          #+#    #+#             */
+/*   Updated: 2017/10/26 21:21:48 by achepurn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int		ft_strlen(char *str);
+char	*ft_strdup(char *src);
+char	*ft_strcpy(char *dst, char *src);
+char	*ft_strncpy(char *dst, char *src, size_t len);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2);
+void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
+char	*ft_strnew(size_t size);
+void	ft_strdel(char **as);
+void	ft_strclr(char *s);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+
+
+#endif
